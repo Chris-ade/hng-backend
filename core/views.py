6aeafbb60e81166729ffa4da7409d1ccded09fc6
Chris Adebiyi <christopherolatunjiade@gmail.com>
2024-07-07 09:08:47 +0100

    Deploying task 1

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.http import HttpResponse, JsonResponse
 import requests
 
-API_KEY = 'f78d1a8ba4eb7ced58dc0669866bee59'
+API_KEY = '0b98a1cd77c455039947dd692915806e'
 
 def index(request):
     return HttpResponse('Hello world.')
@@ -17,15 +17,20 @@ def hello(request):
     location_response = requests.get(f'https://ipinfo.io/{client_ip}/json')
     location_data = location_response.json()
     city = location_data.get('city', 'Unknown location')
-
+    
     # Fetch weather information
     weather_response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}')
     weather_data = weather_response.json()
-    temperature = weather_data['main']['temp']
-
-    response = {
-        "client_ip": client_ip,
-        "location": city,
-        "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
-    }
-    return JsonResponse(response)
+    
+    print(weather_data)
+    
+    if (weather_data.get('cod') == 401 or weather_data.get('cod') == 404):
+        return JsonResponse({'message': weather_data.get('message')})
+    else:
+        temperature = weather_data['main']['temp']
+        response = {
+            "client_ip": client_ip,
+            "location": city,
+            "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
+        }
+        return JsonResponse(response)
